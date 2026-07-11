15dd4e1278f236bb1ddd9443c6899c0a60fb8cb1
AleyJan <aliriayat507@gmail.com>
2026-07-11 17:01:37 +0500

    change hero section

--- frontend/src/components/Route/Hero.jsx
+++ frontend/src/components/Route/Hero.jsx
@@ -12,13 +12,12 @@ const Hero = () => {
     >
       <div className={`${styles.section} w-[90%] 800px:w-[60%]`}>
         <h1 className="text-[28px] leading-[1.2] 800px:text-[44px] 1000px:text-[60px] text-[#3d3a3a] font-[600] capitalize">
-          Best Collection for <br /> home Decoration
+          The Ultimate Convenience
         </h1>
         <p className="pt-4 text-[14px] 800px:text-[16px] font-[Poppins] font-[400] text-[#000000ba]">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Beatae,
-          assumenda? Quisquam itaque exercitationem labore vel, dolore quidem
-          asperiores, laudantium temporibus soluta optio consequatur{" "}
-          <br /> aliquam deserunt officia. Dolorum saepe nulla provident.
+          From everyday essentials to extraordinary finds, get everything you
+          need delivered right to your door. Shop our massive catalog of
+          millions of items at unbeatable prices.
         </p>
         <Link to="/products" className="inline-block">
           <div className="mt-5 bg-black px-5 py-2 800px:px-6 800px:py-3 rounded-md">
